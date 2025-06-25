8a555f5fcd55fcf257e95c2a617bdc5e0771c047
xingkai <xingkai@linhuiba.com>
2025-06-25 11:39:22 +0800

    Revert "fix: css路径支持别名"
    This reverts commit 8946338cc25a2f2f723203414df2a221c261bdbd.

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,5 @@
-const path = require('path')
-
 module.exports = {
   plugins: [
-    require('postcss-import')({
-      resolve(id, basedir, importOptions) {
-        if (id.startsWith('~@/')) {
-          return path.resolve(process.env.UNI_INPUT_DIR || process.cwd(), id.substr(3));
-        } else if (id.startsWith('@/')) {
-          return path.resolve(process.env.UNI_INPUT_DIR || process.cwd(), id.substr(2));
-        } else if (id.startsWith('/') && !id.startsWith('//')) {
-          return path.resolve(process.env.UNI_INPUT_DIR || process.cwd(), id.substr(1));
-        }
-        return id;
-      },
-    }),
     require('autoprefixer')({ remove: false })
   ]
 }
