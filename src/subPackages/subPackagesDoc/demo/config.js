938dec3002c39e7b73067111aa9d201059ccb1da
xingkai <xingkai@linhuiba.com>
2025-06-25 21:20:12 +0800

    引入相关组件

---
+++ src/subPackages/subPackagesDoc/demo/config.js
@@ -0,0 +1,12 @@
+const DEFAULT_CATEGORY = 'other';
+const categoryMap = {
+  'Docs': { label: '文档', seq: 1 },
+  'Basic': { label: '通用', seq: 2 },
+  'Data': { label: '数据', seq: 3 },
+  'Nav': { label: '导航', seq: 4 },
+  'Form': { label: '表单', seq: 5 },
+  'Business': { label: '业务', seq: 6 },
+  'Other': { label: '其他', seq: 7 },
+};
+
+export { DEFAULT_CATEGORY, categoryMap };
